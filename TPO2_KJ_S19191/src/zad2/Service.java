f67d850e6d567fe8225f4b0b1c98fd71074332db
s19191 <s19191@pjwstk.edu.pl>
2020-03-23 11:02:08 +0100

    stan początkowy

--- TPO2_KJ_S19191/src/zad2/Service.java
+++ TPO2_KJ_S19191/src/zad2/Service.java
@@ -6,6 +6,21 @@
 
 package zad2;
 
-
 public class Service {
-}  
+    String kraj;
+    Service(String kraj) {
+        this.kraj = kraj;
+    }
+    String getWeather(String miasto) {
+
+        return null;
+    }
+    Double getRateFor(String kod_waluty) {
+
+        return null;
+    }
+    Double getNBPRate() {
+
+        return null;
+    }
+}
