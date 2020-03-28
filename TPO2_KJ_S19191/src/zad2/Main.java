96ace9eb89cdcfdf53446d71153dc71b42676e19
s19191 <s19191@pjwstk.edu.pl>
2020-03-28 23:39:52 +0100

    gotowy, ewentaulnie można by dodać pole z krajem

--- TPO2_KJ_S19191/src/zad2/Main.java
+++ TPO2_KJ_S19191/src/zad2/Main.java
@@ -6,31 +6,106 @@
 
 package zad2;
 
-
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main extends Application {
+public class Main {
   public static void main(String[] args) {
-    Service s = new Service("Poland");
-    String weatherJson = s.getWeather("Warsaw");
-    Double rate1 = s.getRateFor("USD");
-    Double rate2 = s.getNBPRate();
-    launch(args);
+    Main main = new Main();
+    try {
+      main.showGui();
+    } catch(Exception exc) {
+      JOptionPane.showMessageDialog(null, "Creation failed, " + exc);
+    }
   }
 
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    primaryStage.setTitle("TPO2");
-    TextField textField = new TextField();
-    StackPane stackPane = new StackPane();
-    Scene scene = new Scene(new StackPane(),500,300);
-    primaryStage.setScene(scene);
-    primaryStage.show();
+  public void showGui() {
+    SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        JFrame jf = new JFrame("TPO2");
+        final Service[] service = new Service[1];
+        jf.setPreferredSize(new Dimension(600,300));
+        JTextArea informacje = new JTextArea();
+        informacje.setEditable(false);
+//        JTextArea kraj = new JTextArea("Nie podano jeszcze kraju");
+//        kraj.setEditable(false);
+//        kraj.setBackground(Color.YELLOW);
+        JScrollPane jScrollPane = new JScrollPane(informacje);
+//        jScrollPane.setLayout();
+//        jScrollPane.add(kraj);
+        JButton podajKraj = new JButton("Kliknij, jeżeli chcesz podać kraj");
+        podajKraj.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            String kraj = JOptionPane.showInputDialog("Podaj kraj");
+            if (kraj != null && kraj != "") {
+              Service s = new Service(kraj);
+              if (s.kod != null) {
+                informacje.setText("");
+                service[0] = s;
+              } else {
+                JOptionPane.showMessageDialog(null, "Podano nieprawidłową wartość, taki kraj nie istnieje!");
+              }
+            } else {
+              JOptionPane.showMessageDialog(null, "Podano nieprawidłową wartość, taki kraj nie istnieje!");
+            }
+          }
+        });
+        JButton podajMiasto = new JButton("Kliknij, jeżeli chcesz podać miasto");
+        podajMiasto.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            String miasto = JOptionPane.showInputDialog("Podaj miasto");
+            if (service[0] != null && miasto != null && miasto != "") {
+              String weatherJson = service[0].getWeather(miasto);
+              if (!weatherJson.equals("ex")) {
+                informacje.append(weatherJson + "\n");
+              } else {
+                JOptionPane.showMessageDialog(null, "Podano nieprawidłową wartość, takie miasto nie istnieje!");
+              }
+            } else {
+              JOptionPane.showMessageDialog(null, "Nie podano jeszcze kraju w któym miało by się znajdować miasto");
+            }
+          }
+        });
+        JButton podajWalute = new JButton("Kliknij, jeżeli chcesz, sprawdzić kurs waluty danego kraju wobec innej waluty");
+        podajWalute.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            String waluta = JOptionPane.showInputDialog("Podaj walute");
+            if (service[0] != null && waluta != null && waluta != "") {
+              Double rate1 = service[0].getRateFor(waluta);
+              if (rate1 != 0.0) {
+                informacje.append("Kurs waluty danego kraju wobec waluty podanej jako argument: " + rate1 + "\n");
+              } else {
+                JOptionPane.showMessageDialog(null, "Źle podany kod waluty");
+              }
+              Double rate2 = service[0].getNBPRate();
+              if (rate2 != 0.0) {
+                informacje.append("Kurs złotego wobez waluty danego kraju: " + rate2 + "\n");
+              } else {
+                JOptionPane.showMessageDialog(null, "Wystąpił błąd");
+              }
+            } else {
+              JOptionPane.showMessageDialog(null, "Nie podano jeszcze kraju na rzecz którego można by było sprawdzić kurs waluty");
+            }
+          }
+        });
+        jf.setLayout(new BorderLayout());
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new GridLayout(0,1));
+        jPanel.add(podajKraj);
+        jPanel.add(podajMiasto);
+        jf.add(jPanel,BorderLayout.NORTH);
+        jf.add(jScrollPane,BorderLayout.CENTER);
+        jf.add(podajWalute,BorderLayout.SOUTH);
+        jf.pack();
+        jf.setLocationRelativeTo(null);
+        jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        jf.setVisible(true);
+      }
+    });
   }
 }
--- TPO2_KJ_S19191/src/zad2/Service.java
+++ TPO2_KJ_S19191/src/zad2/Service.java
@@ -6,12 +6,10 @@
 
 package zad2;
 
-import netscape.javascript.JSObject;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import javax.sound.sampled.FloatControl;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -24,92 +22,113 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Service {
-    String kraj;
-    String skrot;
+    final static String klucz = "925c59b6a7c5c50591681595e52ef090";
     Locale locale;
-    Map<String,String> countryCourrency = new HashMap<>();
+    String kraj;
+    String kod;
+    String waluta;
+    Map<String,String> krajKod = new HashMap<>();
+    Map<String,String> kodyWaluty = new HashMap<>();
+
     Service(String kraj) {
-        this.kraj = kraj;
-        locale = new Locale("",kraj);
+        this.kraj = kraj.trim().toUpperCase();
+        locale = new Locale("",this.kraj);
         locale.setDefault(Locale.ENGLISH);
-        for (String loc : Locale.getISOCountries()) {
+        String[] localTab = Locale.getISOCountries();
+        for (String loc : localTab) {
             try {
                 Locale tmp = new Locale("", loc);
-                countryCourrency.put(tmp.getDisplayCountry(), Currency.getInstance(tmp).getCurrencyCode());
+                if (tmp.getDisplayCountry().trim().toUpperCase().equals(locale.getDisplayCountry().trim().toUpperCase())) {
+                    kod = loc;
+                    waluta = Currency.getInstance(tmp).getCurrencyCode();
+                }
+                krajKod.put(tmp.getDisplayCountry().toUpperCase(),loc);
+                kodyWaluty.put(loc, Currency.getInstance(tmp).getCurrencyCode());
             } catch (NullPointerException ex) {
+                //ex.printStackTrace();
                 //do nothing
             }
         }
-        skrot = locale.getDisplayCountry();
     }
     String getURLConnection(String site) throws IOException {
-        //int responseCode = 0;
-        StringBuilder response = new StringBuilder();
-        String lineToRead;
-        URL url = new URL(site);
-        HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
-        httpCon.setRequestMethod("GET");
-        //responseCode = httpCon.getResponseCode();
-        BufferedReader br = new BufferedReader(new InputStreamReader(httpCon.getInputStream()));
-        while ((lineToRead = br.readLine()) != null) {
-            response.append(lineToRead+"\n");
-        }
-        br.close();
-        return response.toString();
-    }
-    String getWeather(String city) {
-        JSONObject jsonObject = null;
         try {
-            jsonObject = new JSONObject(getURLConnection("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+"59045bb3edb0ac8183aee91bb2c18ee9"));
-        } catch (JSONException | IOException e) {
-            e.printStackTrace();
-        }
-        return jsonObject.toString();
-    }
-    Double getRateFor(String currency) {
-        if(countryCourrency.get(locale.getDisplayCountry()).equals(currency))
-            return 1D;
-        else
-        {
-            JSONObject object = null;
-            Double wynik = 0.0;
-            try {
-                object = new JSONObject(getURLConnection("https://api.exchangeratesapi.io/latest?symbols=PLN,"+currency));
-                JSONObject rates = new JSONObject(object.get("rates").toString());
-                wynik = Double.parseDouble(rates.get(countryCourrency.get(locale.getDisplayCountry())).toString());
-            } catch (JSONException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+            StringBuilder response = new StringBuilder();
+            String lineToRead;
+            URL url = new URL(site);
+            HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+            httpCon.setRequestMethod("GET");
+            BufferedReader br = new BufferedReader(new InputStreamReader(httpCon.getInputStream()));
+            while ((lineToRead = br.readLine()) != null) {
+                response.append(lineToRead + "\n");
             }
-
-            return wynik;
+            br.close();
+            return response.toString();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            return "Nie ma takiego miasta";
         }
+    }
+    String getWeather(String miasto) {
+        JSONObject jsonObject;
+        try {
+            String pogoda = "Pogoda dla: " + miasto + "\n";
+            jsonObject = new JSONObject(getURLConnection("http://api.openweathermap.org/data/2.5/weather?q=" + miasto + "&appid=" + klucz));
+            pogoda += "Temperatura: " + jsonObject.getJSONObject("main").getDouble("temp") + "\n";
+            pogoda += "Temperatura minimalna: " + jsonObject.getJSONObject("main").getDouble("temp_min") + "\n";
+            pogoda += "Temperatura maksymalna: " + jsonObject.getJSONObject("main").getDouble("temp_max") + "\n";
+            pogoda += "Temperatura odczuwalna: " + jsonObject.getJSONObject("main").getDouble("feels_like") + "\n";
+            pogoda += "Wilgotność: " + jsonObject.getJSONObject("main").getDouble("humidity") + "\n";
+            pogoda += "Ciśnienie: " + jsonObject.getJSONObject("main").getDouble("pressure") + "\n";
+            return pogoda;
+        } catch (JSONException | IOException ex) {
+            ex.printStackTrace();
+            return "ex";
+        }
+    }
+    Double getRateFor(String s) {
+        Double kurs = 0.0;
+        String podanaWaluta = s.trim().toUpperCase();
+        if(podanaWaluta.equals(waluta)) {
+            kurs = 1.0;
+        } else {
+            JSONObject jsonObject;
+            try {
+                jsonObject = new JSONObject(getURLConnection("https://api.exchangeratesapi.io/latest?base=" + podanaWaluta + "&symbols=" + waluta));
+                JSONObject kursy = new JSONObject(jsonObject.get("rates").toString());
+                kurs = kursy.getDouble(waluta);
+            } catch (JSONException | IOException ex) {
+                //ex.printStackTrace();
+                //do nothing
+            }
+        }
+        return kurs;
     }
     Double getNBPRate() {
-        String xml = null;
-        try {
-            xml = getURLConnection("http://www.nbp.pl/kursy/kursya.html");
-            xml+= getURLConnection("http://www.nbp.pl/kursy/kursyb.html");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String[] tab = xml.split("\n");
-
-        Pattern p = Pattern.compile(">(d+ "+countryCourrency.get(locale.getDisplayCountry())+")<");
-        Matcher m;
-        double rate=-1;
-        for(int i=0; i<tab.length; i++)
-        {
-            m = p.matcher(tab[i]);
-            if(m.matches())
-            {
-                p=Pattern.compile(">(.+)<");
-                m = p.matcher(tab[i+1]);
-                rate=Double.parseDouble(m.group(1));
-                break;
+        Double kurs = 0.0;
+        if (kraj.equals("POLAND")) {
+            kurs = 1.0;
+        } else {
+            try {
+                String strona = getURLConnection("http://www.nbp.pl/kursy/kursya.html");
+                strona+= getURLConnection("http://www.nbp.pl/kursy/kursyb.html");
+                Pattern p = Pattern.compile("<td class=\"bgt\\d.? right\">\\d{1,5} " + waluta + "</td>");
+                Matcher m;
+                String[] tab = strona.split("\n");
+                for(int i=0; i<tab.length; i++) {
+                    m = p.matcher(tab[i].trim());
+                    if(m.matches())
+                    {
+                        p = Pattern.compile("(<td class=\"bgt\\d.? right\">(\\d{1,2},\\d{4})</td>)");
+                        m = p.matcher(tab[i+1].trim());
+                        kurs = Double.parseDouble(m.group(2).replace(",","."));
+                        break;
+                    }
+                }
+            } catch (IOException ex) {
+                //ex.printStackTrace();
+                //do nothing
             }
         }
-        return rate;
+        return kurs;
     }
 }
